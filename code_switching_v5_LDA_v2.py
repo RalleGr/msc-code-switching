299d86ab5a6a675af87fdeefe6a8f6462bc64e6b
Dana Iliescu <dana.iliescu96@gmail.com>
2020-11-23 09:38:50 +0100

    added LDA with chars in count vectorizer

--- code_switching_v5_LDA_v2.py
+++ code_switching_v5_LDA_v2.py
@@ -7,47 +7,26 @@ from tools.utils import is_other
 from tools.utils import printStatus
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation as LDA
+from tools.utils import merge_dictionaries
+import pandas as pd
+WORD_LEVEL_DICTIONARIES_PATH = "./dictionaries/word-level/"
 
 # sources: https://towardsdatascience.com/end-to-end-topic-modeling-in-python-latent-dirichlet-allocation-lda-35ce4ed6b3e0
 # https://github.com/kapadias/mediumposts/blob/master/nlp/published_notebooks/Introduction%20to%20Topic%20Modeling.ipynb
 
-# Get data
-printStatus("Getting test data...")
-filepath = 'datasets/bilingual-annotated/dev.conll'
-file = open(filepath, 'rt', encoding='utf8')
-words = []
-t = []
-for line in file:
-	# Remove empty lines, lines starting with # sent_enum, \n and split on tab
-	if (line.strip() is not '' and '# sent_enum' not in line):
-		line = line.rstrip('\n')
-		splits = line.split("\t")
-		if (splits[1] == 'ambiguous' or splits[1] == 'fw' or splits[1] == 'mixed' or splits[1] == 'ne' or splits[1] == 'unk'):
-			continue
-		else:
-			words.append(splits[0].lower())
-			t.append(splits[1])
-file.close()
+# Get train dictionaries
+printStatus("Getting tokenized sentences...")
+tokenized_sentences_en = pd.read_pickle(r'tokenized_sentences_en.p')
+tokenized_sentences_es = pd.read_pickle(r'tokenized_sentences_es.p')
 
-
-# Remove 'other' words
-printStatus("Removing 'other' data...")
-words_not_other = []
-for word in words:
-	if(not is_other(word)):
-		words_not_other.append(word)
-
-
-# Create an array with two items (equivalent to two 'documents')
-training_data = [" ".join(words_not_other[0:int(len(words_not_other)/2)]),
-				 " ".join(words_not_other[int(len(words_not_other)/2):])]
-
-
+data = tokenized_sentences_en + tokenized_sentences_es
+data = [" ".join(l) for l in data]
+print(data[:3])
 
 # Convert a collection of text documents to a matrix of token counts
 printStatus("Counting words...")
-count_vectorizer = CountVectorizer()
-count_data = count_vectorizer.fit_transform(training_data)
+count_vectorizer = CountVectorizer(analyzer='char', ngram_range=(2, 2))
+count_data = count_vectorizer.fit_transform(data)
 
 
 number_topics = 2
@@ -65,6 +44,24 @@ words_dict = dict()
 for topic_idx, topic in enumerate(lda.components_):
 	for i in topic.argsort()[:-number_words - 1:-1]:
 		words_dict[words1[i]] = topic_idx
+
+# Get test data
+printStatus("Getting test data...")
+filepath = 'datasets/bilingual-annotated/dev.conll'
+file = open(filepath, 'rt', encoding='utf8')
+words = []
+t = []
+for line in file:
+	# Remove empty lines, lines starting with # sent_enum, \n and split on tab
+	if (line.strip() is not '' and '# sent_enum' not in line):
+		line = line.rstrip('\n')
+		splits = line.split("\t")
+		if (splits[1] == 'ambiguous' or splits[1] == 'fw' or splits[1] == 'mixed' or splits[1] == 'ne' or splits[1] == 'unk'):
+			continue
+		else:
+			words.append(splits[0].lower())
+			t.append(splits[1])
+file.close()
 
 # Create the array of predicted classes
 printStatus("Predicting...")
