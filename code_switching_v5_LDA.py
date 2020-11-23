1269640d28bedce60ff5db69c9e1055349561970
Sara Qirko <sara.qirko@finetuning.it>
2020-11-23 14:16:05 +0100

    LDA fixed

--- code_switching_v5_LDA.py
+++ code_switching_v5_LDA.py
@@ -7,6 +7,7 @@ from tools.utils import is_other
 from tools.utils import printStatus
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation as LDA
+from nltk.util import everygrams
 
 # sources: https://towardsdatascience.com/end-to-end-topic-modeling-in-python-latent-dirichlet-allocation-lda-35ce4ed6b3e0
 # https://github.com/kapadias/mediumposts/blob/master/nlp/published_notebooks/Introduction%20to%20Topic%20Modeling.ipynb
@@ -38,51 +39,37 @@ for word in words:
 		words_not_other.append(word)
 
 
-# Create an array with two items (equivalent to two 'documents')
-training_data = [" ".join(words_not_other[0:int(len(words_not_other)/2)]),
-				 " ".join(words_not_other[int(len(words_not_other)/2):])]
+# Convert a collection of words to a matrix of token counts
+printStatus("Counting ngrams...")
+count_vectorizer = CountVectorizer(analyzer='char', ngram_range=(4, 4))
+count_data = count_vectorizer.fit_transform(words_not_other)
 
-
-
-# Convert a collection of text documents to a matrix of token counts
-printStatus("Counting words...")
-count_vectorizer = CountVectorizer()
-count_data = count_vectorizer.fit_transform(training_data)
-
-
-number_topics = 2
-number_words = 1000 # not sure what this is 
 
 # Create and fit the LDA model - where the magic happens :)
 printStatus("Training LDA...")
-lda = LDA(n_components=number_topics)
-lda.fit(count_data)
+number_topics = 2
+lda_model = LDA(n_components=number_topics)
+lda = lda_model.fit_transform(count_data)
 
-# Create a dictionary (word:topic_idx), where topic_idx can be 0 and 1 
-# representing two different language clusters
-words1 = count_vectorizer.get_feature_names()
+
 words_dict = dict()
-for topic_idx, topic in enumerate(lda.components_):
-	for i in topic.argsort()[:-number_words - 1:-1]:
-		words_dict[words1[i]] = topic_idx
+for i in range(len(words_not_other)):
+	if(lda[i][0] > lda[i][1]):
+		topic = 'lang1'
+	else: 
+		topic = 'lang2'
+	words_dict[words_not_other[i]] = topic
 
-# Create the array of predicted classes
-printStatus("Predicting...")
 predicted = []
 for word in words:
 	if(is_other(word)):
 		predicted.append('other')
-	elif (word not in words_dict.keys()): # again not sure how to handle this -_-
-		predicted.append('lang1')
-	elif (words_dict[word] == 0):
-		predicted.append('lang1')
 	else:
-		predicted.append('lang2')
+		predicted.append(words_dict[word])
 
 # Get accuracy
 acc = accuracy_score(t, predicted)
 print(acc)
-# 0.7643618502671089
 
 # Fq score
 f1 = f1_score(t, predicted, average=None)
