59cb37970632992967761c349a2b20af48c4d839
Dana Iliescu <dana.iliescu96@gmail.com>
2020-12-06 21:06:35 +0100

    added results for oracle

--- oracle.py
+++ oracle.py
@@ -11,20 +11,24 @@ from sklearn.metrics import confusion_matrix
 PREDICTIONS_PATH = './results/predictions/'
 
 predictionsFileNames = [
-	'predictions_probabilities.txt',
-	'predictions_2_grams.txt',
-	'predictions_3_grams.txt',
-	'predictions_4_grams.txt',
-	'predictions_5_grams.txt',
-	'predictions_6_grams.txt',
-	'predictions_word_2_grams.txt',
-	'predictions_viterbi_v1.txt',
-	'predictions_viterbi_v2.txt',
-	'predictions_LDA_v1.txt',
-	'predictions_LDA_v2.txt',
-	'predictions_SVM.txt',
-	'predictions_LogisticRegression.txt',
+	'predictions_val_probabilities.txt',
+	'predictions_val_char_5_grams.txt',
+	# 'predictions_val_word_2_grams.txt',
+	'predictions_val_viterbi_v1.txt',
+	'predictions_val_LDA_v2.txt',
+	'predictions_val_SVM.txt',
+	# 'predictions_val_LogisticRegression.txt',
 ]
+
+# predictionsFileNames = [
+# 	'predictions_test_probabilities.txt',
+# 	'predictions_test_char_5_grams.txt',
+# 	# 'predictions_test_word_2_grams.txt',
+# 	'predictions_test_viterbi_v1.txt',
+# 	'predictions_test_LDA_v2.txt',
+# 	'predictions_test_SVM.txt',
+# 	# 'predictions_test_LogisticRegression.txt',
+# ]
 
 # Read all results
 results = []
@@ -35,7 +39,8 @@ for file in predictionsFileNames:
 
 # Get data
 printStatus("Getting test data...")
-filepath = 'datasets/bilingual-annotated/dev.conll'
+filepath = 'datasets/bilingual-annotated/dev.conll' # validation
+# filepath = 'datasets/bilingual-annotated/train.conll' # test
 file = open(filepath, 'rt', encoding='utf8')
 words = []
 t = []
@@ -78,11 +83,34 @@ classes = ['lang1', 'lang2', 'other']
 ConfusionMatrixDisplay(confusion_matrix=conf_matrix,
 					   display_labels=classes).plot(values_format='d')
 plt.savefig('./results/CM/confusion_matrix_oracle.svg', format='svg')
-		
-# Using all outcomes
-# 0.9759222219409069
-# [0.99367525 0.9752323  0.93879938]
+
+# RESULTS
+
+# Validation set
+
+# All outcomes
+# 0.9851887485125452
+# [0.99263341 0.98619647 0.96758294]
 
 # Using probabilities, 5grams, viterbi_v2, SVM, LogisticRegression
-# 0.9680482061928755
-# [0.98435721 0.96493781 0.93879938]
+# 0.9791882927817303
+# [0.98551594 0.97827108 0.96758294]
+
+# Using probabilities, 5grams, viterbi_v2, LDA, SVM
+# 0.9825303187583867
+# [0.98703859 0.98541351 0.96758294]
+
+# Test set
+
+# All outcomes
+# 0.9857390683198123
+# [0.99275907 0.98663358 0.9704282 ]
+
+# Using probabilities, 5grams, viterbi_v2, SVM, LogisticRegression - better than majority voting
+# 0.969717842747079
+# [0.97358582 0.96484662 0.9704282 ]
+
+# Using probabilities, 5grams, viterbi_v2, LDA, SVM - better than majority voting
+# 0.9815041583754274
+# [0.98084909 0.98897098 0.9704282 ]
+
