976d50f0e65505f53e66959ebf4444a341e1dac1
Dana Iliescu <dana.iliescu96@gmail.com>
2021-02-09 15:13:46 +0100

    refactor probabilities code for predictions for original test data

--- code_switching_v2_char_ngrams.py
+++ code_switching_v2_char_ngrams.py
@@ -13,10 +13,18 @@ import sys
 
 CHAR_LEVEL_DICTIONARIES_PATH = "./dictionaries/character-level/"
 
+# Get language codes from keyboard
+if len(sys.argv) == 1:
+	print("Please enter n value for n-grams")
+	print("Please enter evaluation dataset: 'dev', 'test' or 'test-original'")
+	exit(1)
+
 n = int(sys.argv[1])
 if n!=2 and n!=3 and n!=4 and n!=5 and n!=6:
 	print("n should be 2, 3, 4, 5 or 6")
 	exit(1)
+
+evaluation_dataset = sys.argv[2]
 
 # Get dictionaries
 print_status("Getting dictionaries...")
@@ -35,22 +43,38 @@ model_es = NGramModel(n)
 model_es.load_ngrams_freq(frequency_es_dict)
 
 # Get data
-print_status("Getting test data...")
-# filepath = 'datasets/bilingual-annotated/dev.conll' # validation
-filepath = 'datasets/bilingual-annotated/test.conll' # test
+if (evaluation_dataset == 'dev'):
+	filepath = './datasets/bilingual-annotated/dev.conll' # validation
+if (evaluation_dataset == 'test'):
+	filepath = './datasets/bilingual-annotated/test.conll' # test
+if (evaluation_dataset == 'test-original'):
+	filepath = './datasets/bilingual-annotated/test-original.conll' # original test set from LinCE
+
 file = open(filepath, 'rt', encoding='utf8')
 words = []
 t = []
-for line in file:
-	# Remove empty lines, lines starting with # sent_enum, \n and split on tab
-	if (line.strip() is not '' and '# sent_enum' not in line):
-		line = line.rstrip('\n')
-		splits = line.split("\t")
-		if (splits[1]=='ambiguous' or splits[1]=='fw' or splits[1]=='mixed' or splits[1]=='ne' or splits[1]=='unk'):
-			continue
+
+if (evaluation_dataset != 'test-original'):
+	# Own dev/test set
+	for line in file:
+		# Remove empty lines, lines starting with # sent_enum, \n and split on tab
+		if (line.strip() is not '' and '# sent_enum' not in line):
+			line = line.rstrip('\n')
+			splits = line.split("\t")
+			if (splits[1]=='ambiguous' or splits[1]=='fw' or splits[1]=='mixed' or splits[1]=='ne' or splits[1]=='unk'):
+				continue
+			else:
+				words.append(splits[0])
+				t.append(splits[1])
+else:
+	# Original test set
+	for line in file:
+		# Remove empty lines, lines starting with # sent_enum, \n and split on tab
+		if (line.strip() is not ''):
+			token = line.rstrip('\n')
+			words.append(token.lower())
 		else:
-			words.append(splits[0])
-			t.append(splits[1])
+			words.append('')
 file.close()
 
 # Choose language with highest probability for each word based on ngrams
@@ -59,23 +83,30 @@ predictions_dict = dict()
 counter = 0
 print_status("Classifying...")
 for word in words:
-	word = word.lower()
-	if is_other(word):
-		lang = 'other'
-	else:
-		prob_en = model_en.get_word_log_prob(word)
-		prob_es = model_es.get_word_log_prob(word)
-		if (prob_en >= prob_es):
-			lang = 'lang1'
+	if (word != ''):
+		word = word.lower()
+		if is_other(word):
+			lang = 'other'
 		else:
-			lang = 'lang2'
-	
-	y.append(lang)
-	predictions_dict[word] = lang
+			prob_en = model_en.get_word_log_prob(word)
+			prob_es = model_es.get_word_log_prob(word)
+			if (prob_en >= prob_es):
+				lang = 'lang1'
+			else:
+				lang = 'lang2'
+		
+		y.append(lang)
+		predictions_dict[word] = lang
 
-	if counter % 10000 == 0:
-		print(f"{counter} of {len(words)}")
-	counter+=1
+		if counter % 10000 == 0:
+			print(f"{counter} of {len(words)}")
+		counter+=1
+	else:
+		y.append('')
+
+if (evaluation_dataset == 'test-original'):
+	save_predictions(y, './results/predictions/predictions_test_original_char_' + str(n) + '_grams.txt')
+	exit(1)
 
 # Get accuracy
 acc = accuracy_score(t, y)
@@ -92,8 +123,10 @@ ConfusionMatrixDisplay(confusion_matrix=conf_matrix, display_labels=classes).plo
 plt.savefig('./results/CM/confusion_matrix_char_' + str(n) + '_grams.svg', format='svg')
 
 # Save model output
-# save_predictions(predictions_dict, './results/predictions/predictions_val_char_' + str(n) + '_grams.txt')
-save_predictions(predictions_dict, './results/predictions/predictions_test_char_' + str(n) + '_grams.txt')
+if (evaluation_dataset == 'dev'):
+	save_predictions(predictions_dict, './results/predictions/predictions_val_char_' + str(n) + '_grams.txt')
+if (evaluation_dataset == 'test'):
+	save_predictions(predictions_dict, './results/predictions/predictions_test_char_' + str(n) + '_grams.txt')
 
 
 # RESULTS
